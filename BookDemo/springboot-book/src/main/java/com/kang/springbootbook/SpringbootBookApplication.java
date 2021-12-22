9cfdac35429282545765183c207e8eefe9e027f4
320kang <houyongkang@bupt.edu.cn>
2021-12-22 18:49:06 +0800

    2021.12.22 bookdemo init

---
+++ BookDemo/springboot-book/src/main/java/com/kang/springbootbook/SpringbootBookApplication.java
@@ -0,0 +1,13 @@
+package com.kang.springbootbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBookApplication.class, args);
+    }
+
+}
---
+++ BookDemo/springboot-book/src/test/java/com/kang/springbootbook/SpringbootBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kang.springbootbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
